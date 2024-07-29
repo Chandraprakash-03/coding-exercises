f6d7cd868ba1774f521f6ac94fb186ad287d9380
Chandraprakash <131947246+Chandraprakash-03@users.noreply.github.com>
2024-07-29 14:58:46 +0530

    initial commit

--- exercise-2/src/Main.java
+++ exercise-2/src/Main.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
  *  Handles user commands and provides an interactive console interface.
 */
 public class Main {
-    public static void main(String[] args) {
 
         // Initialization
-        VirtualClassroomManager manager = new VirtualClassroomManager();
-        CommandHandler commandHandler = new CommandHandler(manager);
-        Logger logger = Logger.getInstance();
+        private static final VirtualClassroomManager manager = new VirtualClassroomManager();
+        private static final CommandHandler commandHandler = new CommandHandler(manager);
+        private static final Logger logger = Logger.getInstance();
+        
+
+    public static void main(String[] args) {
 
         //Console menu
         System.out.println("Virtual Classroom Manager");
         System.out.println("Commands");
         System.out.println("1. add_classroom <class_name>");
-        System.out.println("2. add_student <student_id> <class_name>");
+        System.out.println("2. add_student <student_id> <student_name> <class_name>");
         System.out.println("3. schedule_assignment <class_name> <assignment_id> <description> <due_date>");
         System.out.println("4. submit_assignment <student_id> <class_name> <assignment_id>");
         System.out.println("5. list_students <class_name>");
@@ -28,28 +30,30 @@ public class Main {
         System.out.println("11. list_assignment_for_student <student_id>");
         System.out.println("Type 'exit' to quit");
 
+        commandProcessing();
+       
+    }
+
+    /**
+     * Method to get and process user inputs and commands.
+     */
+    private static void commandProcessing(){
         Scanner scanner = new Scanner(System.in);
-        String command;
+        System.out.println("Enter command: ");
+        String command = scanner.nextLine();
 
-        // loop to handle user commands
-        while(true) {
-            System.out.println("Enter command: ");
-            command = scanner.nextLine();
-
-            if("exit".equalsIgnoreCase(command.trim())) {
-                break;
-            }
-
+        if(!"exit".equalsIgnoreCase(command.trim())){
             try{
                 commandHandler.handleCommand(command);
-            }catch (Exception e){
+            }
+            catch(Exception e){
                 logger.error("Error while handling command: " + command);
                 logger.logException(e);
             }
+            commandProcessing();
         }
-
-        // Closing Logger and Scanner
-        logger.close();
         scanner.close();
+        logger.close();
     }
+   
 }
